c0b456fe94ce681a244d33a9b5b20976d3ae74f6
Tamunonengiyeofori <tkennwariso@gmail.com>
2022-12-14 15:36:39 +0100

    Added async function to create an admin at server startup and delete server shutdown

---
+++ app/models_test.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine 
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:22of22in22@localhost:5432/loan_service"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id  = Column(Integer, autoincrement=True, primary_key=True)
+    jobs = relationship("Job", back_populates="user")
+    
+class Audio(Base):
+    __tablename__ = "audios"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    # agent_id = Column(Integer, autoincrement=True, server_default=56, unique=True)
+    audio_record = Column(String(80))
+    job = relationship("Job", back_populates="audio_record", uselist=False)
+
+class Job(Base):
+    __tablename__ = "jobs"
+    id = Column(Integer, autoincrement=True, primary_key=True)    
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User", back_populates="jobs")
+    audio_id = Column(Integer, ForeignKey("audios.id", ondelete="CASCADE"), unique=True)
+    audio_record= relationship("Audio", back_populates="job")
+    # transcript = relationship("Transcript", back_populates="job", uselist=False)
+
+class Transcript(Base):
+    __tablename__ = "transcripts"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    transcript_id = Column(Integer, ForeignKey("jobs.id" , ondelete="CASCADE"), unique=True) 
+    transcript_text = Column(String(255))
+    # job_id = Column(Integer, ForeignKey("jobs.id", ondelete="CASCADE"), unique=True)
+    # job = relationship("Job", back_populates="transcript")
+    
+    
+class Sentiment(Base):
+    __tablename__ = "sentiments"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    sentiment_id = Column(Integer, ForeignKey("transcripts.transcript_id"))
+    response = relationship("Response", back_populates="sentiment")
+    response_id = Column(Integer, ForeignKey("response.id"))
+    
+class Response(Base):
+    __tablename__ = "response"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    friendly_score = Column(Integer)
+    customer_satisfaction = Column(Integer)
+    sentiment = relationship("Sentiment", back_populates="response", uselist=False)
+    
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,15 +1,54 @@
 from .. import models, schemas
-from fastapi import FastAPI, status, HTTPException, Depends, APIRouter
-from ..database import get_db
+from fastapi import FastAPI, status, HTTPException, Depends, APIRouter, Response
+from ..database import get_db, engine
 from .. import models, utils, Oauth2
 from sqlalchemy.orm import Session
 from typing import List
+from faker import Faker
+
 
 
 user_router = APIRouter(
     prefix="/users",
     tags = ["Users"]
 )
+
+fake = Faker()
+
+ADMIN_USER = ""
+
+# Create an Admin at startup
+@user_router.on_event("startup")
+async def create_startup_admin():
+    global ADMIN_USER
+    admin_email = fake.email()
+    admin_password = "123456"
+    with Session(engine) as session:
+        user = session.query(models.User).filter(models.User.email == admin_email).first()
+        if user is not None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail=f"User with email: {admin_email} already exists")
+        admin_password = utils.hash_passowrd(admin_password)
+        new_user = models.User(email=admin_email,
+                            password=admin_password,
+                            is_admin=True)
+        session.add(new_user)
+        ADMIN_USER = new_user.email
+        session.commit()
+        print(ADMIN_USER)
+        session.refresh(new_user)
+        
+#Delete admin at server shutdown
+@user_router.on_event("shutdown")
+async def delete_admins():
+    global ADMIN_USER
+    with Session(engine) as session:
+        admin_user = session.query(models.User).filter(models.User.email == ADMIN_USER).first()
+        if admin_user:
+            session.delete(admin_user)
+            session.commit()
+  
+
 
 # Create a User
 @user_router.post("/signup", status_code=status.HTTP_201_CREATED)
@@ -37,7 +76,7 @@ def get_all_users(db: Session = Depends(get_db),
     if user.is_admin:
         all_users = db.query(models.User).all()
         return all_users
-    raise HTTPException(
+    return Response(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="You are not authorized to perform this action"
+        content="You are not authorized to perform this action"
     )
